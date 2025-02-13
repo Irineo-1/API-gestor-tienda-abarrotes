7bdf26fe5e8b56f29a06e47faf2e9edee70777d1
Irineo-1 <76635517+Irineo-1@users.noreply.github.com>
2025-02-13 15:34:49 -0600

    ADD: se agrego JWT a las rutas de ventas y xd de usuaios

--- rutas/usuarios.py
+++ rutas/usuarios.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from baseModels.IUsuario import IUsuario
 from models.Usuarios import Usuarios
+from .token import verificar_token
 
 router = APIRouter(prefix="/usuario", tags=["Usuario"])
 
--- rutas/ventas.py
+++ rutas/ventas.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.Ventas import Ventas
 from baseModels.IVenta import IVenta
+from .token import verificar_token
 
 router = APIRouter(prefix="/venta", tags=["Ventas"])
 
 @router.get("/{date}")
-def get_ventas(date: str):
+def get_ventas(date: str, usuario: dict = Depends(verificar_token)):
     return Ventas.getVentas(date)
 
 @router.post("/")
-async def add_venta(request: IVenta):
+async def add_venta(request: IVenta, usuario: dict = Depends(verificar_token)):
     Ventas.addVenta(request)
     return {"response": "OK"}
