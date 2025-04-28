115e7b5c211af8959f153df64a7bf73a3d666883
Irineo-1 <76635517+Irineo-1@users.noreply.github.com>
2025-04-27 22:14:46 -0600

    Update models and routes

--- models/Usuarios.py
+++ models/Usuarios.py
@@ -4,5 +4,21 @@ from baseModels.IUsuario import IUsuario
 
 class Usuarios: 
 
+	__Tabla_usuarios: str = "usuarios"
+	__Tabla_typo_usuarios: str = "typo_usuarios"
+
 	def Autenticacion(request: IUsuario):
 		return generar_token(request.usuario)
+
+	def GetUsuarios():
+		cnn = getConexion()
+		cursor = cnn.cursor(dictionary=True)
+		sql: str = f"""
+			SELECT user.id, user.usuario, typouser.typo FROM {Usuarios.__Tabla_usuarios} user 
+			INNER JOIN {Usuarios.__Tabla_typo_usuarios} typouser 
+			on typouser.id = user.typo
+		"""
+		cursor.execute(sql)
+		res = cursor.fetchall()
+		cursor.close()
+		return res
--- rutas/usuarios.py
+++ rutas/usuarios.py
@@ -5,6 +5,11 @@ from .token import verificar_token
 
 router = APIRouter(prefix="/usuario", tags=["Usuario"])
 
+@router.get("/")
+def GetUsuarios(usuario: dict = Depends(verificar_token)):
+	usuarios = Usuarios.GetUsuarios()
+	return usuarios
+
 @router.post("/login")
 def Autenticacion(request: IUsuario):
 	token = Usuarios.Autenticacion(request)
